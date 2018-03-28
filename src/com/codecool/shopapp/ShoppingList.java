838dfc591ff07e4ce4a9c90c3d876ebd21d5288c
Dóra Surmann <surmann.dora@gmail.com>
2018-03-28 11:57:42 +0200

    list item addition

--- src/com/codecool/shopapp/ShoppingList.java
+++ src/com/codecool/shopapp/ShoppingList.java
@@ -14,15 +14,24 @@ public class ShoppingList {
 
     
     public static void main(String[] args) {
-
         String[] shoppingItems = new String[50];
+        Boolean isStillShopping = true;
         int iterator = 0;
-        shoppingItems[iterator++] = "Eggs";
-        shoppingItems[iterator++] = "Milk";
-        shoppingItems[iterator++] = "Soda Water";
-        displayList(shoppingItems);
+        while (isStillShopping) { 
+            System.out.println(isStillShopping);
+            System.out.println();
+            System.out.println("Enter item name:");
+            Scanner userInput = new Scanner(System.in);
+            String itemName = userInput.nextLine();   
+            if (itemName.equals("x")) {
+                isStillShopping = false;
+                break;
+            } 
+            shoppingItems[iterator++] = itemName;
+            displayList(shoppingItems, iterator);
+        }
     }
-    public static void displayList(String shoppingItems[]) {
+    public static void displayList(String shoppingItems[], int iterator) {
         System.out.println("___________________________________");
         for (int i = 0; i < iterator; i++){
             System.out.println("|   " + shoppingItems[i] + stringMultiply(" ", (30-shoppingItems[i].length())) + "|" );        
