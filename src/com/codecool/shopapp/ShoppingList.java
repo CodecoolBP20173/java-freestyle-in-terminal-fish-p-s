ba748201fcdeb6c63c9182f42ece4ea47ed0389d
Dóra Surmann <surmann.dora@gmail.com>
2018-03-28 15:01:54 +0200

    Revert "design for main menu added"
    This reverts commit bccd10836f0bbf20d4f4bcbb532a379d29a2ba3a.

--- src/com/codecool/shopapp/ShoppingList.java
+++ src/com/codecool/shopapp/ShoppingList.java
@@ -23,19 +23,14 @@ public class ShoppingList {
 
         while (userOpt != 2) {
             System.out.println("");
-            System.out.println("----- Fish & Ps Shopping List------");
-            System.out.println("");
-            System.out.println("(1) Create new list. ");
+            System.out.println("----- Welcome to My Shopping List------");
+            System.out.println("(1) Create a new shopping list. ");
             System.out.println("(2) Exit. ");
             userOpt = menuOption.nextInt();  
             if (userOpt == 1) {
-                //obj.moveTo(5,10);
-                obj.clearScreen();
                 createList(obj, shoppingItems);
             }
             if (userOpt == 2) {
-                obj.clearScreen();
-                System.out.println("------Thanks for using our application!------");
                 break;
             }
         }
@@ -47,7 +42,6 @@ public class ShoppingList {
         while (isStillShopping) { 
             System.out.println();
             System.out.println("Enter item name:");
-            System.out.println("(to finish your list, enter x)");
             Scanner userInput = new Scanner(System.in);
             String itemName = userInput.nextLine();   
             if (itemName.equals("x")) {
@@ -73,7 +67,7 @@ public class ShoppingList {
             }
 
         for (int i = 0; i < iterator; i++){
-            System.out.println("| - " + shoppingItems[i] + stringMultiply(" ", (25-shoppingItems[i].length())) + "|" );        
+            System.out.println("|   " + shoppingItems[i] + stringMultiply(" ", (25-shoppingItems[i].length())) + "|" );        
         }
         if (isStillShopping == false){
             System.out.println("|           Thank you! Mom   |");
