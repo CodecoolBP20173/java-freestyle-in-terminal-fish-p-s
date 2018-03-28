980c394cc291bb58f3206d781a643d5866d4d3a9
Dóra Surmann <surmann.dora@gmail.com>
2018-03-28 11:16:18 +0200

    refactored function for  displaying list

--- src/com/codecool/shopapp/ShoppingList.java
+++ src/com/codecool/shopapp/ShoppingList.java
@@ -14,19 +14,23 @@ public class ShoppingList {
 
     
     public static void main(String[] args) {
-        
+
         String[] shoppingItems = new String[50];
         int iterator = 0;
         shoppingItems[iterator++] = "Eggs";
         shoppingItems[iterator++] = "Milk";
         shoppingItems[iterator++] = "Soda Water";
+        displayList(shoppingItems);
+    }
+    public static void displayList(String shoppingItems[]) {
         System.out.println("___________________________________");
         for (int i = 0; i < iterator; i++){
             System.out.println("|   " + shoppingItems[i] + stringMultiply(" ", (30-shoppingItems[i].length())) + "|" );        
         }
         System.out.println("-----------------------------------");
-    }
-    
+    }    
+
+
     public static String stringMultiply(String s, int n){
     StringBuilder sb = new StringBuilder();
     for(int i = 0; i < n; i++){
